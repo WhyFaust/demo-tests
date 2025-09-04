d685e9ad1b323e60a7d9af44a4634a33e0c34c7a
Faust <baferpro@gmail.com>
2025-09-04 12:31:03 +0300

    fix & test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,33 +1,33 @@
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
 
 
-URL = "https://www.saucedemo.com/"
-USERNAME = "standard_user"
-PASSWORD = "secret_sauce"
+def test_login(browser):
+    login_page = LoginPage(browser)
+    inventory_page = InventoryPage(browser)
+
+    # открываем страницу логина
+    login_page.open()
+
+    # логинимся
+    login_page.login("standard_user", "secret_sauce")
+
+    # проверяем, что открылась страница товаров
+    assert inventory_page.is_opened()
 
 
-def test_login(browser: WebDriver) -> None:
-    browser.get(URL)
+def test_logout(browser):
+    login_page = LoginPage(browser)
+    inventory_page = InventoryPage(browser)
 
-    browser.find_element(By.ID, "user-name").send_keys(USERNAME)
-    browser.find_element(By.ID, "password").send_keys(PASSWORD)
-    browser.find_element(By.ID, "login-button").click()
+    # логинимся
+    login_page.open()
+    login_page.login("standard_user", "secret_sauce")
+    assert inventory_page.is_opened()
 
-    products_title = browser.find_element(By.CLASS_NAME, "title").text
-    assert products_title == "Products"
-    assert len(browser.find_elements(By.CLASS_NAME, "inventory_item")) > 0
+    # разлогиниваемся
+    inventory_page.logout()
 
-def test_logout(browser: WebDriver) -> None:
-    browser.get(URL)
-
-    browser.find_element(By.ID, "user-name").send_keys(USERNAME)
-    browser.find_element(By.ID, "password").send_keys(PASSWORD)
-    browser.find_element(By.ID, "login-button").click()
-
-    browser.find_element(By.ID, "react-burger-menu-btn").click()
-    browser.find_element(By.ID, "logout_sidebar_link").click()
-
-    assert browser.current_url == URL
-    assert browser.find_element(By.ID, "login-button").is_displayed()
+    # проверяем, что снова открылась форма логина
+    assert login_page.is_displayed()
